6065f83c4e9d947bf46ed7dc45654d5936d1101b
23520904 <23520904@ms.uit.edu.vn>
2025-08-13 09:48:37 +0700

    update .1

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -17,6 +17,7 @@ const SignInAuthButton = () => {
       variant="secondary"
       className="w-full text-white border-zinc-200 h-11"
     >
+      <img src="/google.png" alt="Google" className="size-5" />
       Continue with Google
     </Button>
   );
